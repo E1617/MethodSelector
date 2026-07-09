5b9f74df27589ad5d36b0cee333033648ccae48d
erick121416 <ericknieto929@gmail.com>
2026-07-08 21:28:22 -0500

    se añadio el modulo de historial y descargar pdf

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
   path('biblioteca/', views.PintarBiblioteca, name = 'biblioteca'),
   path('evaluar/', views.evaluar, name='evaluar'),
   path('comparar/', views_comparation.ViewComparative, name='comparar'),
+  path('Historial/', views.historial_completo, name='historial'),
   path('api/obtener-criterios/', views.obtener_criterios_fase, name='obtener_criterios'),
   path('api/obtener-metodos/', views.obtener_metodos_por_categoria, name='obtener_metodos'),
   path('api/guardar-evaluacion/', views.guardar_evaluacion_completa, name='guardar_evaluacion'),
+  path('historial/reporte/<int:id_proyecto>/', views.generar_reporte, name='generar_reporte'),
+  path('api/obtener-justificacion/<int:id_proyecto>/', views.obtener_justificacion, name='api_justificacion'),
 ]
--- core/views_comparation.py
+++ core/views_comparation.py
@@ -22,26 +22,22 @@ def GetDataMethodByID(MetodoID):
     return None
 
 def ViewComparative(request):
-    # 1. Siempre traemos todos los métodos disponibles para que los checkboxes sigan ahí
     todos_los_metodos = GetAllMethods()
     
     metodos_comparar = []
     filas_comparativas = []
     mostrar_tabla = False
 
-    # 2. Si el usuario presionó el botón (POST), procesamos la tabla
     if request.method == "POST":
         ids_seleccionados = request.POST.getlist('method_ids')
         
         if ids_seleccionados:
             mostrar_tabla = True
-            # Obtenemos los datos de la BD para cada ID seleccionado
             for mid in ids_seleccionados:
                 datos = GetDataMethodByID(mid)
                 if datos:
                     metodos_comparar.append(datos)
 
-            # Extraemos las filas dinámicas tal como lo hicimos antes
             atributos = metodos_comparar[0].keys() if metodos_comparar else []
             for atributo in atributos:
                 valores_de_esta_fila = []
@@ -53,7 +49,6 @@ def ViewComparative(request):
                     'valores': valores_de_esta_fila
                 })
 
-    # 3. Enviamos todo al mismo contexto
     context = {
         'todos_los_metodos': todos_los_metodos,
         'metodos_seleccionados': metodos_comparar,
